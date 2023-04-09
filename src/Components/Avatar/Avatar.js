f57e6148cadbf074a09f64960b1e22f6d310cf70
YaredBG <Yared_Gari@Mckinsey.com>
2023-04-08 20:09:57 -0700

    resolved error in a console

--- src/Components/Avatar/Avatar.js
+++ src/Components/Avatar/Avatar.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./Avatar.css";
 
-const Avatar = ({ className = "", AvatarUrl }) => {
+const Avatar = ({ className = "profileImg", AvatarUrl }) => {
   return (
-      <div className={className || "profileImg"}>
+      <div className={className}>
         <img
           src={AvatarUrl || "https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584__340.png"}
           alt="avatar"
